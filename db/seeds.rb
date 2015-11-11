9fef74e4f7cf0d1d89bdd654e12c6a0b7f9c17bf
Cezar Azevedo <chep.chaf@gmail.com>
2015-11-11 17:02:39 -0300

    Basic Layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Book.destroy_all
+
+
+Book.create!([{
+  title: "Hyperion",
+  author: "Dan Simmons",
+  description: "Probably my favorite science fiction book (and series) I've ever read. Hyperion is written in a style similar
+to The Canterbury Tales, in which a series of stories are told by the main characters. Each story is a gem in itself, but alude
+to the larger storyline. The score of the story is ambitious - spanning time, planets religion and love.",
+  amazon_id: "0553283685"
+  },
+  {
+    title: "The name of the wind",
+    author: "Patrick Rothfuss",
+    description: "My favorite book ever, The name of the wind tells the history of Kvothe a man who is known by his history and is considered
+a legend, this book is narred by Kvothe itself since the beggining of his life... and maybe the end.",
+    amazon_id: "0756404746"
+  }
+])

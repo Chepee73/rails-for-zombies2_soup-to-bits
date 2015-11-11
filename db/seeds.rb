83e899512495d8ce77f4c366d37fbe169c0fe0c9
Cezar Azevedo <chep.chaf@gmail.com>
2015-11-11 17:29:50 -0300

    Add Books

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,13 +14,17 @@ Book.create!([{
   description: "Probably my favorite science fiction book (and series) I've ever read. Hyperion is written in a style similar
 to The Canterbury Tales, in which a series of stories are told by the main characters. Each story is a gem in itself, but alude
 to the larger storyline. The score of the story is ambitious - spanning time, planets religion and love.",
-  amazon_id: "0553283685"
+  amazon_id: "0553283685",
+  rating: 4,
+  finished_on: nil
   },
   {
     title: "The name of the wind",
     author: "Patrick Rothfuss",
     description: "My favorite book ever, The name of the wind tells the history of Kvothe a man who is known by his history and is considered
 a legend, this book is narred by Kvothe itself since the beggining of his life... and maybe the end.",
-    amazon_id: "0756404746"
+    amazon_id: "0756404746",
+    rating: 5,
+    finished_on: 2.months.ago
   }
 ])
